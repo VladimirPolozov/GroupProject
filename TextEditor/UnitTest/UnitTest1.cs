b361b1d016c11e54dd6d239470bdd2bb0d39b4fb
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2024-06-11 15:11:22 +0700

    added singleton and unit tests for it
    Singleton in class FileHandler

--- TextEditor/UnitTest/UnitTest1.cs
+++ TextEditor/UnitTest/UnitTest1.cs
@@ -1,15 +1,60 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reflection;
 using TextEditor;
 
-namespace UnitTest
+namespace TextEditorTests
 {
     [TestClass]
-    public class UnitTest1
+    public class FileHandlerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Setup()
         {
+            // Используем рефлексию для сброса состояния синглтона перед каждым тестом
+            FieldInfo instance = typeof(FileHandler).GetField("_instance", BindingFlags.Static | BindingFlags.NonPublic);
+            instance.SetValue(null, null);
+        }
+
+        [TestMethod]
+        public void GetInstance_ShouldReturnSameInstance()
+        {
+            var instance1 = FileHandler.GetInstance();
+            var instance2 = FileHandler.GetInstance();
+
+            Assert.AreEqual(instance1, instance2);
+        }
+
+        [TestMethod]
+        public void OpenFile_ShouldReadFileCorrectly()
+        {
+            var fileHandler = FileHandler.GetInstance();
+            var fileFormat = new TxtFileFormat();
+            fileHandler.SetFileFormat(fileFormat);
+            string filePath = "test.txt";
+            string expectedContent = "Hello, World!";
+
+            // Имитация записи в файл
+            System.IO.File.WriteAllText(filePath, expectedContent);
+
+            var content = fileHandler.OpenFile(filePath);
+
+            Assert.AreEqual(expectedContent, content);
+        }
+
+        [TestMethod]
+        public void SaveFile_ShouldWriteFileCorrectly()
+        {
+            var fileHandler = FileHandler.GetInstance();
+            var fileFormat = new TxtFileFormat();
+            fileHandler.SetFileFormat(fileFormat);
+            string filePath = "test_save.txt";
+            string contentToSave = "This is a saved file content";
+
+            fileHandler.SaveFile(filePath, contentToSave);
+            var savedContent = System.IO.File.ReadAllText(filePath);
+
+            Assert.AreEqual(contentToSave, savedContent);
         }
     }
 }
